17ec22cf49bd71fdd3d6fdc8143f55a6824bb6f9
José Alexandre Filho <josealexandre301428@gmail.com>
2022-10-09 18:35:39 -0300

    Feat: implementa telas de login e registro

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import NotFound from './pages/notFound';
+import LoginForm from './pages/login/LoginForm';
+import RegisterForm from './pages/registro/RegisterForm';
 
 function App() {
   return (
     <BrowserRouter basename={ process.env.PUBLIC_URL }>
       <main className="main">
         <Routes>
-          {/*  */}
+          <Route exact path="/" element={ <Navigate to="/login" /> } />
+          <Route exact path="/login" element={ <LoginForm /> } />
+          <Route exact path="/register" element={ <RegisterForm /> } />
+          <Route exact path="/products" element={ <NotFound /> } />
         </Routes>
       </main>
     </BrowserRouter>
